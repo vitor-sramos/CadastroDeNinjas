bd8fce76962fe104e1f3ad8d2f59f7d44dce0f57
Vitor de Souza Ramos <vitor.sramos04@gmail.com>
2025-06-11 12:28:48 -0300

    Cria uma entidade no banco de dados apartir do model Ninja.

---
+++ src/main/java/com/vitor/dev/CadastroDeNinjas/Controllers/NinjaController.java
@@ -0,0 +1,15 @@
+package com.vitor.dev.CadastroDeNinjas.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class NinjaController {
+
+    @GetMapping(value = "/boasVindas")
+    public String boasVindas() {
+        return "Está é a minha primeira mensagem nesta rota!";
+    }
+}
---
+++ src/main/java/com/vitor/dev/CadastroDeNinjas/Model/Ninja.java
@@ -0,0 +1,23 @@
+package com.vitor.dev.CadastroDeNinjas.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_ninja")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Ninja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // vai gerer id com números seuquênciais.
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+}
