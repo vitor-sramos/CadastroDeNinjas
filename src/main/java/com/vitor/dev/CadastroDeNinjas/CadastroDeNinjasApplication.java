078c03fdcae5db666df643c9f02c78f9eb47cb4c
Vitor de Souza Ramos <vitor.sramos04@gmail.com>
2025-06-11 11:58:37 -0300

    first commit

---
+++ src/main/java/com/vitor/dev/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.dev.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
