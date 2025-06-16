70c677879e4c516da4defd55e459529fc317a229
Vitor de Souza Ramos <vitor.sramos04@gmail.com>
2025-06-16 12:20:34 -0300

    Criando migration para Ninja e Missoes.

--- src/main/java/com/vitor/dev/CadastroDeNinjas/Missoes/Model/Missoes.java
+++ src/main/java/com/vitor/dev/CadastroDeNinjas/Missoes/Model/Missoes.java
@@ -2,11 +2,19 @@ package com.vitor.dev.CadastroDeNinjas.Missoes.Model;
 
 import com.vitor.dev.CadastroDeNinjas.Ninja.Model.Ninja;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Missoes {
 
     @Id
